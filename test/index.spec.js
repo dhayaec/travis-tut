248f3a9562d80970d151452d07635e8f19ed286f
Dhayanandhan.R <dhaya.r@partner.samsung.com>
2019-01-22 10:53:01 +0530

    add function

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,12 @@
-const expect = require('chai').expect
-const server = require('../src/index');
+const expect = require("chai").expect;
+const server = require("../src/index");
+const { add } = require("../src/utils");
 
-describe('test', () => {
-  it('should return a string', () => {
-    expect('ci with travis').to.equal('ci with travis');
+describe("test", () => {
+  it("should return a string", () => {
+    expect("ci with travis").to.equal("ci with travis");
+  });
+  it("add two numbers", () => {
+    expect(add(3, 3)).to.equal(6);
   });
 });
